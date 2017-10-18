6aa8b4d34eb77ea18900291dc3232f745f37c712
goluchm <michal.goluch@live.umcs.edu.pl>
2017-10-18 15:07:25 +0200

    Add files via upload

---
+++ Zadanie2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Imie: ";
+read imie;
+
+echo "Nazwisko: ";
+read nazwisko;
+
+echo "PESEL: ";
+
+read -n 11 pesel
+
+if [ ${#pesel}
+
+echo 'Imie: '${imie}' nazwisko: ' ${nazwisko}' PESEL: ' ${pesel}
